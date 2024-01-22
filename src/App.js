d6c4a2b76256c5b4878fa932df8b0a0428550411
mae <mae@maes-MacBook-Pro.local>
2024-01-21 16:57:48 -0800

    fix main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import zhiboma from './ZHIBOMA.png';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <img src={zhiboma}/>
         <h1>ZHIBO MA</h1>
         <p>
           Welcome to my site.
