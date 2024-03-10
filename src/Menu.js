3c873391698ff5f649e93e7ff9fa02f5161cc08e
mae <ema@live.nmhu.edu>
2024-03-09 21:14:11 -0800

    add menu

--- src/Menu.js
+++ src/Menu.js
@@ -9,18 +9,18 @@ const Menu = () => {
   };
 
   return (
-    <div className={`menu ${isOpen ? 'open' : ''}`}>
+    <div className="menu">
       <div className="brand">Zhibo Ma</div>
       <div className="hamburger-menu" onClick={toggleMenu}>
-        <div className="line"></div>
-        <div className="line"></div>
-        <div className="line"></div>
+        <div className={`line ${isOpen ? 'open' : ''}`}></div>
+        <div className={`line ${isOpen ? 'open' : ''}`}></div>
+        <div className={`line ${isOpen ? 'open' : ''}`}></div>
       </div>
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Contact</a></li>
+      <ul className={isOpen ? 'open' : ''}>
+        <li><a href="#">Leadership</a></li>
+        <li><a href="#">Sport & Music</a></li>
+        <li><a href="#">Publications</a></li>
+        <li><a href="#">Projects</a></li>
       </ul>
     </div>
   );
