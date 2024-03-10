38d73651d05aff7c5eeae3bb3596d3dd6f8eda71
mae <ema@live.nmhu.edu>
2024-03-09 20:59:39 -0800

    fix color

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import zhiboma from './ZHIBOMA.png';
-
+import Menu from './Menu';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Menu />
       <img src={zhiboma}/>
         <h1>ZHIBO MA</h1>
         <p>
---
+++ src/Menu.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import './Menu.css';
+
+const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={`menu ${isOpen ? 'open' : ''}`}>
+      <div className="brand">Zhibo Ma</div>
+      <div className="hamburger-menu" onClick={toggleMenu}>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Menu;
